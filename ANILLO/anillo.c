58e673107a59c8fb28bca24fb2f8fecccef87ae6
LAPTOP-U7QCHFHM\Unioscar <oscarlabaya@gmail.com>
2021-04-22 09:48:08 +0200

    anillo

---
+++ ANILLO/anillo.c
@@ -0,0 +1,47 @@
+#include <mpi.h>
+#include <stdio.h>
+
+int main(int argc,char** argv){
+    int myrank, numprocs, i;
+    int dato;
+
+    MPI_Status estado;
+    MPI_Init(&argc,&argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+    MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+    int desde, hacia;
+
+    if(myrank == 0){
+        //Enviamos el dato al primer proceso
+        printf("Introduce un entero: \n");
+        scanf("%d",&dato);
+        hacia = myrank + 1;
+        desde = numprocs-1;
+        printf("Voy a transmitir el entero: %d \n",dato);
+        MPI_Send(&dato,1,MPI_INT,hacia,8,MPI_COMM_WORLD);
+        MPI_Recv(&dato,1,MPI_INT,desde,8,MPI_COMM_WORLD,&estado);
+        printf("Soy el proceso %d, el entero que he recibido es: %d \n", myrank,dato);
+
+    }
+    else if(myrank == numprocs-1){
+        desde = myrank - 1;
+        hacia = 0;
+        MPI_Recv(&dato,1,MPI_INT,desde,8,MPI_COMM_WORLD,&estado);
+        printf("Soy el proceso %d, el entero que he recibido es: %d \n", myrank,dato);
+        dato += 1;
+        MPI_Send(&dato,1,MPI_INT,hacia,8,MPI_COMM_WORLD);
+    }
+    else
+    {
+        desde = myrank - 1;
+        hacia = myrank + 1;
+        MPI_Recv(&dato,1,MPI_INT,desde,8,MPI_COMM_WORLD,&estado);
+        printf("Soy el proceso %d, el entero que he recibido es: %d \n", myrank,dato);
+        dato += 1;
+        MPI_Send(&dato,1,MPI_INT,hacia,8,MPI_COMM_WORLD);
+
+    }
+
+    MPI_Finalize();
+
+}
