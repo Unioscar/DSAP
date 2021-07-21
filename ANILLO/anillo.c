c7570165942089af8bf80084edaaa3190e54cc85
Unioscar <60978878+Unioscar@users.noreply.github.com>
2021-07-21 22:28:04 +0200

    Update anillo.c

--- ANILLO/anillo.c
+++ ANILLO/anillo.c
@@ -6,9 +6,9 @@ int main(int argc,char** argv){
     int dato;
 
     MPI_Status estado;
-    MPI_Init(&argc,&argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
-    MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+    MPI_Init(&argc,&argv); //Iniciamos MPI
+    MPI_Comm_rank(MPI_COMM_WORLD, &myrank); //Devuelve el rango del proceso en el que nos encontramos 0...N
+    MPI_Comm_size(MPI_COMM_WORLD, &numprocs); //Devuelve el numero de procesos totales
     int desde, hacia;
 
     if(myrank == 0){
@@ -18,7 +18,7 @@ int main(int argc,char** argv){
         hacia = myrank + 1;
         desde = numprocs-1;
         printf("Voy a transmitir el entero: %d \n",dato);
-        MPI_Send(&dato,1,MPI_INT,hacia,8,MPI_COMM_WORLD);
+        MPI_Send(&dato,1,MPI_INT,hacia,8,MPI_COMM_WORLD); 
         MPI_Recv(&dato,1,MPI_INT,desde,8,MPI_COMM_WORLD,&estado);
         printf("Soy el proceso %d, el entero que he recibido es: %d \n", myrank,dato);
 
@@ -42,6 +42,6 @@ int main(int argc,char** argv){
 
     }
 
-    MPI_Finalize();
+    MPI_Finalize(); //Finalizamos el proceso
 
 }
